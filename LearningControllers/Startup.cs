c1170e0ce04cd4afa0c4367c84f37ff45066c78b
Gustaw Beźnicki <g.beznicki@gmail.com>
2020-02-18 16:00:42 +0100

    prepared initial configuration in startrup class

--- LearningControllers/Startup.cs
+++ LearningControllers/Startup.cs
@@ -16,25 +16,19 @@ namespace LearningControllers
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
