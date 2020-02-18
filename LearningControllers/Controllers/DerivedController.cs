a10f8418bde2137510ac6c7d524589ecd474bc44
Gustaw Beźnicki <g.beznicki@gmail.com>
2020-02-18 16:36:58 +0100

    added two simple controllers

---
+++ LearningControllers/Controllers/DerivedController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningControllers.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ViewResult Index() => View("Result", $"This is a derived controller");
+    }
+}
